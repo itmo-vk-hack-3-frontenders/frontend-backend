1133866c74e140688fd52a8983faea4506e153fe
alexgorynych <alex0n0xela@gmail.com>
2023-04-15 18:56:46 +0300

    added store

---
+++ app/apps/frontend/src/app/store/index.ts
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+// import { taskModel } from "entities/task";
+
+export const store = configureStore({
+  reducer: {
+    device: ,
+    stat: ,
+    // tasks: taskModel.reducer,
+  },
+});
---
+++ app/apps/frontend/src/shared/api/services/api.service.ts
@@ -0,0 +1,16 @@
+import { axios } from "../axios";
+
+const fetchDevices = async () => {
+  const response = axios.get("");
+  return;
+};
+
+const fetchChart = async () => {
+  const response = axios.get("");
+  return;
+};
+
+export {
+  fetchDevices,
+  fetchChart,
+};
